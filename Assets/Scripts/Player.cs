c4a17a746b643b70d469ddd2fe55307ae3c2be2f
Michael Svoboda <michael.svoboda1@stud.fh-campuswien.ac.at>
2021-09-27 18:48:45 +0200

    Added new player asset, background and game ui

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
@@ -9,10 +11,17 @@ public class Player : MonoBehaviour
     private bool jumpKeyWasPressed;
     private float horizontalInput;
     private Rigidbody rigidBodyComponent;
+    private int score = 0;
+    [SerializeField]
+    public Text scoreText;
+    [SerializeField]
+    public Text finishText;
 
     // Start is called before the first frame update
     void Start()
     {
+        scoreText.text = "Score: 0";
+        finishText.text = "";
         rigidBodyComponent = GetComponent<Rigidbody>();
     }
 
@@ -46,9 +55,26 @@ public class Player : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("On Trigger Enter");
+        Debug.Log(other.tag);
         if(other.gameObject.layer == 9)
         {
             Destroy(other.gameObject);
+            score++;
+            scoreText.text = "Score: " + score;
+
         }
+
+        if (other.gameObject.tag == "Finish")
+        {
+            StartCoroutine(FinishRoutine());
+        }
+    }
+
+    IEnumerator FinishRoutine()
+    {
+        finishText.text = "You have won!";
+        yield return new WaitForSeconds(4);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
